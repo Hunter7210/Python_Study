75593d4e44c8e046b353fbd740be856fc0a71e2d
Hunter7210 <heitorrmsilva@gmail.com>
2024-02-07 14:32:14 -0300

    07/02/2024

---
+++ PrimeirosPassos/TiposDeDados/Tipo_Dados.py
@@ -0,0 +1,45 @@
+#Tipos de variaveis
+# int, str, float ,bool, list, tuple, dict
+
+# Para converter um valor utiliza o Casting
+# numero = 42
+# texto = str(numero)  # Converte o número para string
+
+
+# INT
+num1 = 1
+num2 = 3
+
+# FLOAT
+num1 = 2.3
+pi = 3,14
+
+# STRING
+text1 = "Heitor"
+
+# Boolean
+ligado = True
+desligado = False
+
+# Listas e Tupla 
+# Lista(Array que aumenta)
+# Tupla(Não aumenta)
+
+# Listas Utilizam COLCHETES[]
+frutas = ["maçã, banana, uva"]
+numeros = [1,2,3,4,5]
+
+# Adicionando elementos
+frutas.append("Laranja")
+print(frutas)
+
+# Imprimir um valor especifico utilizar o index 
+print(frutas[1])
+
+# Tuplas utilizam PARENTESÊS()
+coordenadas = (4,5)
+cores_rgb = (255,0,0)
+
+print(coordenadas)
+
+print(coordenadas[1])
--- PrimeirosPassos/Variaveis/ExemVariaveis.py
+++ PrimeirosPassos/Variaveis/ExemVariaveis.py
@@ -1,6 +1,30 @@
 
-salario = 3000;
-bonus = 500
-salario_total = salario + bonus
+# salario = 3000
+# bonus = 500
+# salario_total = salario + bonus
 
-print(salario_total)
+# print(salario_total)
+
+
+""" nome = "Heitor"
+sobre = "Rodrigo"
+nome_todo = nome +" "+ sobre
+
+print(nome_todo)"""
+
+# cont = 0
+# cont = cont+1 ou cont =+1
+# print(cont)
+""" 
+idade = 10
+if idade >=18:
+    pode_votar = True
+else:
+    pode_votar= False
+
+print(pode_votar) """
+""" 
+Uma variavel pode ser chamada deste modo:
+
+idade: int = 25
+nome: str = "Alice" """
