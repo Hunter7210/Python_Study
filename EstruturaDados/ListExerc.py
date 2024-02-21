d69da0bc5b1280acdc88056793cc4ddbfb53a8d6
Hunter7210 <heitorrmsilva@gmail.com>
2024-02-21 16:56:59 -0300

    21/02/2024

--- EstruturaDados/ListExerc.py
+++ EstruturaDados/ListExerc.py
@@ -1,9 +1,12 @@
 
 #Ativ 1
 
-from random import Random
-from ssl import RAND_add
+import random
 import statistics
+from textwrap import indent
+
+import numpy as np
+from seaborn import plotting_context
 
 
 numeros = [1,2,3,4,5]
@@ -38,23 +41,62 @@ for i in chars:
     for j in vogais:
         if i == j:
             cont += 1
-    else:
-        print(i)
-
 print(f"Seu vetor tem {cont} consoantes")
 
 #Ativ 5
-Random.
 
-nu =
+numbers20 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
-numbers20 = []
-print(nu)
+par =[]
+impar=[]
 
-""" for i in numbers20:
+for i in numbers20:
+    if (i%2) == 0:
+        par.append(i)
+    else:
+        impar.append(i)
+
+print(numbers20)
+print(par)
+print(impar)
+
+
+#Ativ 6 
+condicao = 0
+while condicao<2:
+
+    aluno = input("Bem vindo, digite aqui o nome do aluno")
+    matematica = int(input("Digite aqui as notas de Matematica:"))
+    bio = int(input("Digite aqui as notas de Biologia:"))
+    fis = int(input("Digite aqui as notas de Fisica:"))
+    qui = int(input("Digite aqui as notas de Quimica:"))
     
+    soma = int(matematica+bio+fis+qui)
+    media_not = soma/4
+
+    notas = [media_not]
+    print(media_not)
+    print(notas)
 
 
+    contNotas = 0
+    for i in notas:
+        if i>=7.0:
+            print(f"O aluno {aluno} esta aprovado!") 
+            contNotas+=1
+        else:
+            print(f"O aluno {aluno} esta reprovado!")
+
+    condicao+=1        
 
 
- """
+#Ativ7
+    
+num5 = [1,2,3,4,5]
+
+soma = sum(num5)
+mult = np.multiply(nump5)
+
+print(num5)
+print(soma)
+print(mult)
