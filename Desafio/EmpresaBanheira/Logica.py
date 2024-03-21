fb62735ba237fbf0b6f2f6edfad805e941ffce33
Hunter7210 <heitorrmsilva@gmail.com>
2024-03-20 21:55:01 -0300

    20/03/2024

--- Desafio/EmpresaBanheira/Logica.py
+++ Desafio/EmpresaBanheira/Logica.py
@@ -1,28 +1,60 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+
+# import seaborn as sns
 import yaml
 
-
 # Carregar dados do arquivo YAML
-with open('Desafio/EmpresaBanheira/empresa.yaml', 'r') as file:
+with open("Desafio/EmpresaBanheira/empresa.yaml", "r") as file:
     dados = yaml.safe_load(file)
 
-# Ler conjunto de dados em DataFrame Pandas
-# Verificar o comprimento de cada coluna
-for key, value in dados.items():
-    print(f"Comprimento da coluna '{key}': {len(value)}")
-    
 # Criar DataFrame com os dados
-df = pd.DataFrame(dados)
+df = pd.DataFrame(dados["vendas"])
+
+# Mostando a tabela
+print(df)
+
+
+# Plotando gráfico de contagem de vendas X Mês
+
+# Encontrando o Mês de cada
+df["month"] = pd.DatetimeIndex(df["data"]).month
+print(df["month"])
+
+# Calculando a contagem
+contagemVendas = df["month"].value_counts()
+print(f"A contagem é {contagemVendas}")
+
+
+# Plotar histograma da idade com Matplotlib
+plt.figure(figsize=(8, 6))  # Criar uma figura com tamanho específico
+graficoContMe = plt.bar(contagemVendas.index, contagemVendas.values, width = 0.5, align='center')
+plt.title('Vendas X Mês')  # Definir título do gráfico
+plt.xlabel('Mês')  # Definir rótulo do eixo x
+plt.ylabel('Contagem de Vendas')  # Definir rótulo do eixo y
+
+plt.bar_label(graficoContMe, fmt="%.01f", label_type="edge")  # Mostrar o gráfico
+plt.show()
+
+# Media de itens vendidos por Venda
+
+# Media de itens vendidos
+media_itens_vendidos = df["quantidade"].mean()
+
+print(media_itens_vendidos)
+
 
 
 
 # Plotar histograma da idade com Matplotlib
 plt.figure(figsize=(8, 6))  # Criar uma figura com tamanho específico
-plt.hist(df['idade'], bins=5, color='skyblue', edgecolor='black')  # Plotar histograma
-plt.title('Histograma da Idade - Matplotlib')  # Definir título do gráfico
-plt.xlabel('Idade')  # Definir rótulo do eixo x
-plt.ylabel('Frequência')  # Definir rótulo do eixo y
-plt.grid(True)  # Habilitar grade no gráfico
-plt.show()  # Mostrar o gráfico
+graficotes = plt.bar(media_itens_vendidos, df['produto'], width = 0.5, align='center')
+plt.title('Vendas X Mês')  # Definir título do gráfico
+plt.xlabel('Mês')  # Definir rótulo do eixo x
+plt.ylabel('Contagem de Vendas')  # Definir rótulo do eixo y
+
+plt.bar_label(graficotes, fmt="%.01f", label_type="edge")  # Mostrar o gráfico
+plt.show()
+
+
--- ExerciciosMatriz/DesafioExtraMatriz.py
+++ ExerciciosMatriz/DesafioExtraMatriz.py
@@ -8,7 +8,7 @@ preco_medio_acao1 = np.array([50, 52, 48, 55, 53, 51, 49, 50, 54, 52])
 preco_medio_acao2 = np.array([120, 122, 118, 125, 123, 121, 119, 120, 124, 122])
 # Número de ações que o investidor possui de cada empresa
 acoes_acao1 = 100  # Exemplo: o investidor possui 100 ações da Ação 1
-acoes_acao2 = 50   # Exemplo:o investidor possui 50 ações da Ação 2
+acoes_acao2 = 50  # Exemplo:o investidor possui 50 ações da Ação 2
 # Calculando o valor do investimento em cada dia
 
 investim_diario_acao1 = acoes_acao1 * preco_medio_acao1
@@ -26,18 +26,17 @@ cont = 1
 # Exibindo os resultados diariamente
 for i in patrimo_invest:
     print(f"Dia{cont}: {i}")
-    cont+=1
+    cont += 1
 
-array_dia = np.array([1,2,3,4,5,6,7,8,9,10])
-#plot o gráfico da evolução patrimonial do investidor
-plt.title('Grafico de investimento')
-plt.plot(array_dia, patrimo_invest , marker='o', color='b')
-plt.plot(array_dia, investim_diario_acao1, marker='o', color='r')
-plt.plot(array_dia, investim_diario_acao2, marker='o', color='g')
-plt.ylabel('Patrimônio')
-plt.xlabel('Ações')
-plt.grid('true')
+array_dia = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+# plot o gráfico da evolução patrimonial do investidor
+plt.title("Grafico de investimento")
+plt.plot(array_dia, patrimo_invest, marker="o", color="b")
+plt.plot(array_dia, investim_diario_acao1, marker="o", color="r")
+plt.plot(array_dia, investim_diario_acao2, marker="o", color="g")
+plt.ylabel("Patrimônio")
+plt.xlabel("Ações")
+plt.grid("true")
 
 plt.show()
-
-
